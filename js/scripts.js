bffc9d3aa6df96200567f5d16b12253d656e40b5
Noah Horwitz and Peter Podrid <noah@>
2019-10-28 15:20:43 -0700

    add additional options to use refactored js

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,30 @@ $(document).ready(function() {
     var almostSentence = []
 
     var originalSentence = $("input#sentence").val().split(" ");
-    var finalSentence = originalSentence.map(function(word){
-      if (word.length < 3) {
-        return;
-      }
-      else {
-        almostSentence.push(word);
-      }
-    })
+
+// ***** THIS ONE IS TOO LONG *****
+    // var finalSentence = originalSentence.map(function(word){
+    //   if (word.length < 3) {
+    //     return;
+    //   }
+    //   else {
+    //     almostSentence.push(word);
+    //   }
+    // })
+
+
+// ***** THIS ONE IS ONLY A LITTLE LONG *****
+    // var almostSentence = originalSentence.filter(function(word){
+    //   return word.length >= 3;
+    // });
+
+
+// ***** THIS ONE IS JUST RIGHT! *****
+    var almostSentence = originalSentence.filter(word => word.length >= 3);
+
+
+
+
     console.log("almostSentence: ",almostSentence);
       var evenCloser = almostSentence.reverse().join(" ");
     console.log("evenCloser: ",evenCloser);
